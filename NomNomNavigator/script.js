4a1674f479dd2d40bba5a1c17e02059b373d5463
Pravin <144076247+PravinRram@users.noreply.github.com>
2024-01-24 15:19:24 +0800

    pushed from VSC

---
+++ NomNomNavigator/script.js
@@ -0,0 +1,22 @@
+.location.href = "dessert.html"
+    }
+
+    // fill in the remaining options
+}
+
+function randomDessert() {
+    var randomNumber = Math.floor(Math.random() * 6)
+
+    if (randomNumber == 0) {
+        document.getElementById("randomName").innerHTML = "Cheesecake"
+        document.getElementById("randomRecipe").innerHTML = "Cheese, Biscuits"
+    }
+    else if (randomNumber == 1) {
+        document.getElementById("randomName").innerHTML = "Chendol"
+        document.getElementById("randomRecipe").innerHTML = "Jelly, Ice, Red Bean"
+    }
+    else {
+        document.getElementById("randomName").innerHTML = "Ice Cream"
+        document.getElementById("randomRecipe").innerHTML = "Milk, Chocolate"
+    }
+}
