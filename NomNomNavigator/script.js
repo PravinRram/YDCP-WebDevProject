3e55dd6f6abb6b61dc16830897c2984b5bf35a13
Pravin <144076247+PravinRram@users.noreply.github.com>
2024-01-24 17:42:24 +0800

    h

--- NomNomNavigator/script.js
+++ NomNomNavigator/script.js
@@ -3,7 +3,7 @@ const splash = document.querySelector('.splash');
 document.addEventListener('DOMContentLoaded', (e)=>(
     setTimeout(() => {
         splash.classList.add('display-none');
-    }, "2000")
+    }, 2000)
 ))
 
 function goToCategory() {
