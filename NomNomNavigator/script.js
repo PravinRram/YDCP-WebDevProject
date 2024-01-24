3f3935ae83ff64edff9cfd1bd8d02cc38d9ec017
Pravin <144076247+PravinRram@users.noreply.github.com>
2024-01-24 17:25:15 +0800

    e

--- NomNomNavigator/script.js
+++ NomNomNavigator/script.js
@@ -1,3 +1,11 @@
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', (e)=>(
+    setTimeout(() => {
+        splash.classList.add('display-none');
+    }, 2000)
+))
+
 function goToHomeScreen() {
     setTimeout(() => {
         window.location.href = "home.html"
