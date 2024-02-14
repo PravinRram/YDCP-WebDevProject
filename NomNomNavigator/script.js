825f023b20fa86d6a1f8893827d64f60e806f350
Pravin <144076247+PravinRram@users.noreply.github.com>
2024-02-14 17:24:12 +0800

    ....

--- NomNomNavigator/script.js
+++ NomNomNavigator/script.js
@@ -1,117 +1,116 @@
 const chineseMain = [
-    { name: "Char Siu Noodles", description: "test", imageSrc: "PravinRram/YDCP-WebDevProject/NomNomNavigator/images/CharSiuNoodles.jpeg", ytLink: "" }, 
-    { name: "Chicken Rice", description: "test", imageSrc: "PravinRram/YDCP-WebDevProject/NomNomNavigator/images/CharSiuNoodles.jpeg", ytLink: "" }, 
-    { name: "Peking Duck", description: "test", imageSrc: "PravinRram/YDCP-WebDevProject/NomNomNavigator/images/CharSiuNoodles.jpeg", ytLink: "" },
+    { name: "Char Siu Noodles", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/CharSiuNoodles.jpeg", ytLink: "" }, 
+    { name: "Chicken Rice", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/ChickenRice.jpeg", ytLink: "" }, 
+    { name: "Peking Duck", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/PekingDuck.jpeg", ytLink: "" },
+    { name: "Peking Duck", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/ChineseEggplantSalad.jpeg", ytLink: "" }
 ];
 const chineseDrink = [
-    { name: "Yunnan Coffee", description: "test", imageSrc: "", ytLink: "" }, 
-    { name: "Black Tea", description: "test", imageSrc: "", ytLink: "" }, 
-    { name: "Kombucha", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Yunnan Coffee", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/YunnanCoffee.webp", ytLink: "" }, 
+    { name: "Black Tea", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Kombucha.webp", ytLink: "" }, 
+    { name: "Kombucha", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/BlackTea.jpeg", ytLink: "" }
 ];
 const chineseSnack = [
-    { name: "Bak Kwa", description: "test", imageSrc: "", ytLink: "" }, 
-    { name: "Dumplings", description: "test", imageSrc: "", ytLink: "" }, 
-    { name: "Spring Rolls", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Bak Kwa", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/BakKwa.jpeg", ytLink: "" }, 
+    { name: "Dumplings", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Dumpling.jpeg", ytLink: "" }, 
+    { name: "Spring Rolls", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/SpringRoll.jpeg", ytLink: "" }
 ];
 const chineseDessert = [
-    { name: "Chendol", description: "test", imageSrc: "", ytLink: "" }, 
-    { name: "Almond Jelly", description: "test", imageSrc: "", ytLink: "" }, 
-    { name: "Mooncake", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Chendol", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Chendol.jpeg", ytLink: "" }, 
+    { name: "Mooncake", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/MoonCake.jpeg", ytLink: "" }
 ];
 
 const indianMain = [
-    { name: "Butter Chicken", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Chicken Biryani", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Masala Dosa", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Paneer Masala", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Butter Chicken", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/ButterChicken.webp", ytLink: "" },
+    { name: "Chicken Biryani", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/ChickenBiriyani.jpeg", ytLink: "" },
+    { name: "Masala Dosa", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/MasalaDosa.jpeg", ytLink: "" },
+    { name: "Paneer Masala", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/PaneerMasala.jpeg", ytLink: "" }
 ];
 const indianDrink = [
-    { name: "Mango Lassi", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Buttermilk", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Chai", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Badam Milk", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Mango Lassi", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/MangoLassi.jpeg", ytLink: "" },
+    { name: "Buttermilk", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/ButterMilk.jpeg", ytLink: "" },
+    { name: "Chai", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Chai.webp", ytLink: "" },
+    { name: "Badam Milk", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/BadamMilk.jpeg", ytLink: "" }
 ];
 const indianSnack = [
-    { name: "Samosa", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Murukku", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Pakora", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Dhokla", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Samosa", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Samosa.webp", ytLink: "" },
+    { name: "Murukku", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Murukku.jpeg", ytLink: "" },
+    { name: "Pakora", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Pakora.jpeg", ytLink: "" },
+    { name: "Dhokla", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Dhokla.webp", ytLink: "" }
 ];
 const indianDessert = [
-    { name: "Gulab Jamun", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Rasmalai", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Kulfi", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Gulab Jamun", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/GulabJamun.jpeg", ytLink: "" },
+    { name: "Rasmalai", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Rasmalai.jpeg", ytLink: "" },
+    { name: "Kulfi", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Kulfi.jpeg", ytLink: "" }
 ];
 
 const japaneseMain = [
-    { name: "Ramen", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Teriyaki Chicken Rice", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Udon", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Katsu", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Ramen", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Ramen.jpeg", ytLink: "" },
+    { name: "Teriyaki Chicken Rice", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/TeriyakiChickenRice.webp", ytLink: "" },
+    { name: "Udon", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Udon.jpeg", ytLink: "" },
+    { name: "Katsu", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Katsu.jpeg", ytLink: "" }
 ];
 const japaneseDrink = [
-    { name: "Amazake", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Sake", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Matcha", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Royal Milk Tea", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Amazake", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Amazake.jpeg", ytLink: "" },
+    { name: "Sake", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Sake.webp", ytLink: "" },
+    { name: "Matcha", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Matcha.jpeg", ytLink: "" },
+    { name: "Royal Milk Tea", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/RoyalMilkTea.webp", ytLink: "" }
 ];
 const japaneseSnack = [
-    { name: "Onigiri", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Caramel Corn", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Pocky", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Rice Crackers", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Onigiri", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Onigiri.jpeg", ytLink: "" },
+    { name: "Caramel Corn", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/CaramelCorn.png", ytLink: "" },
+    { name: "Pocky", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Pocky.jpeg", ytLink: "" },
+    { name: "Rice Crackers", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/RiceCrackers.jpeg", ytLink: "" }
 ];
 const japaneseDessert = [
-    { name: "Sakura Mochi", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Hokkaido Baked Cheese Tarts", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Raindrop Cake", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Purin", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Sakura Mochi", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/SakuraMochi.jpeg", ytLink: "" },
+    { name: "Hokkaido Baked Cheese Tarts", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Tart.jpeg", ytLink: "" },
+    { name: "Raindrop Cake", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/RaindropCake.jpeg", ytLink: "" },
+    { name: "Purin", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Purin.jpeg", ytLink: "" }
 ];
 
 const mexicanMain = [
-    { name: "Tamale", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Burrito", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Quesadilla", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Enchiladas", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Tamale", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Tamales.jpeg", ytLink: "" },
+    { name: "Burrito", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Burrito.webp", ytLink: "" },
+    { name: "Quesadilla", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Quesadilla.webp", ytLink: "" },
+    { name: "Enchiladas", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Enchiladas.webp", ytLink: "" }
 ];
 const mexicanDrink = [
-    { name: "Rompope", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Hibiscus Tea", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Carajillo", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Rompope", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Rompope.webp", ytLink: "" },
+    { name: "Hibiscus Tea", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/HibiscusTea.jpeg", ytLink: "" },
+    { name: "Carajillo", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Carajillo.webp", ytLink: "" }
 ];
 const mexicanSnack = [
-    { name: "Nachos", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Tacos", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Chimichanga", description: "test", imageSrc: "", ytLink: "" } 
+    { name: "Nachos", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/BadamMilk.jpeg", ytLink: "" },
+    { name: "Tacos", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/BadamMilk.jpeg", ytLink: "" },
+    { name: "Chimichanga", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/BadamMilk.jpeg", ytLink: "" } 
 ];
 const mexicanDessert = [
-    { name: "Mexican Hot Chocolate Pie", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Churros", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Flan Mexicano", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Mexican Hot Chocolate Pie", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Nachos.jpeg", ytLink: "" },
+    { name: "Churros", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Churros.webp", ytLink: "" },
+    { name: "Flan Mexicano", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/FlanMexicano.jpeg", ytLink: "" }
 ];
 
 const westernMain = [
-    { name: "Meat Pie", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Steak", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Turkey", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Fish and Chips", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Meat Pie", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/MeatPie.jpeg", ytLink: "" },
+    { name: "Steak", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Steak.jpeg", ytLink: "" },
+    { name: "Turkey", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Turkey.jpeg", ytLink: "" },
+    { name: "Fish and Chips", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/FishAndChips.webp", ytLink: "" }
 ];
 const westernDrink = [
-    { name: "Hot Chocolate", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Lemonade", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Root Beer", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Hot Chocolate", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/HotChocolate.jpeg", ytLink: "" },
+    { name: "Lemonade", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Lemonade.jpeg", ytLink: "" },
+    { name: "Root Beer", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/RootBeer.webp", ytLink: "" }
 ];
 const westernSnack = [
-    { name: "Salad", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Maple Pancakes", description: "test", imageSrc: "", ytLink: "" },
-    { name: "French Fries", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Salad", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Salad.jpeg", ytLink: "" },
+    { name: "Maple Pancakes", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/MaplePancakes.jpeg", ytLink: "" },
 ];
 const westernDessert = [
-    { name: "Chocolate Lava Cake", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Tiramisu", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Cinnamon Rolls", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Donuts", description: "test", imageSrc: "", ytLink: "" },
-    { name: "Gingerbread Cookies", description: "test", imageSrc: "", ytLink: "" }
+    { name: "Chocolate Lava Cake", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/ChocolateLavaCake.jpeg", ytLink: "" },
+    { name: "Tiramisu", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Tiramisu.jpeg", ytLink: "" },
+    { name: "Cinnamon Rolls", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/CinnamonRolls.webp", ytLink: "" },
+    { name: "Donuts", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Donuts.jpeg", ytLink: "" },
+    { name: "Gingerbread Cookies", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/GingerbreadCookies.jpeg", ytLink: "" }
 ];
 
 function generateRandomFood() {
