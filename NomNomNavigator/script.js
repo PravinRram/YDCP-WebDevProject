887cf5b19af1e4f6a6bc413a9b3a32efa410b0ef
Pravin <144076247+PravinRram@users.noreply.github.com>
2024-02-19 11:44:00 +0800

    Part 2 descriptions

--- NomNomNavigator/script.js
+++ NomNomNavigator/script.js
@@ -5,9 +5,9 @@ const chineseMain = [
     { name: "Chinese Eggplant Salad", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/ChineseEggplantSalad.jpeg", ytLink: "" }
 ];
 const chineseDrink = [
-    { name: "Yunnan Coffee", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/YunnanCoffee.webp", ytLink: "" }, 
-    { name: "Black Tea", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Kombucha.webp", ytLink: "" }, 
-    { name: "Kombucha", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/BlackTea.jpeg", ytLink: "" }
+    { name: "Yunnan Coffee", description: "Coffee grown in the Yunnan province of China, known for its smooth and slightly fruity flavor profile.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/YunnanCoffee.webp", ytLink: "" }, 
+    { name: "Black Tea", description: "A type of tea made from oxidized tea leaves, offering a robust flavor and caffeine kick.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Kombucha.webp", ytLink: "" }, 
+    { name: "Kombucha", description: "A fermented tea beverage known for its tangy flavor and potential health benefits due to its probiotic content.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/BlackTea.jpeg", ytLink: "" }
 ];
 const chineseSnack = [
     { name: "Bak Kwa", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/BakKwa.jpeg", ytLink: "" }, 
@@ -15,7 +15,7 @@ const chineseSnack = [
     { name: "Spring Rolls", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/SpringRoll.jpeg", ytLink: "" }
 ];
 const chineseDessert = [
-    { name: "Chendol", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Chendol.jpeg", ytLink: "" }, 
+    { name: "Chendol", description: "A Southeast Asian dessert consisting of green rice-flour jelly, coconut milk, palm sugar syrup, and sometimes red beans and shaved ice.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Chendol.jpeg", ytLink: "" }, 
     { name: "Mooncake", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/MoonCake.jpeg", ytLink: "" }
 ];
 
@@ -26,10 +26,10 @@ const indianMain = [
     { name: "Paneer Masala", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/PaneerMasala.jpeg", ytLink: "" }
 ];
 const indianDrink = [
-    { name: "Mango Lassi", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/MangoLassi.jpeg", ytLink: "" },
-    { name: "Buttermilk", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/ButterMilk.jpeg", ytLink: "" },
-    { name: "Chai", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Chai.webp", ytLink: "" },
-    { name: "Badam Milk", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/BadamMilk.jpeg", ytLink: "" }
+    { name: "Mango Lassi", description: "A popular Indian drink made from ripe mangoes, yogurt, milk, and sugar, blended into a smooth and creamy concoction.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/MangoLassi.jpeg", ytLink: "" },
+    { name: "Buttermilk", description: "A tangy and refreshing dairy beverage made from the liquid left over after churning butter from cream.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/ButterMilk.jpeg", ytLink: "" },
+    { name: "Chai", description: "A spiced tea beverage originating from the Indian subcontinent, typically made with black tea, milk, sugar, and spices such as cardamom, cinnamon, and ginger.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Chai.webp", ytLink: "" },
+    { name: "Badam Milk", description: "A creamy Indian beverage made from milk infused with ground almonds, flavored with saffron and cardamom.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/BadamMilk.jpeg", ytLink: "" }
 ];
 const indianSnack = [
     { name: "Samosa", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Samosa.webp", ytLink: "" },
@@ -38,27 +38,27 @@ const indianSnack = [
     { name: "Dhokla", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Dhokla.webp", ytLink: "" }
 ];
 const indianDessert = [
-    { name: "Gulab Jamun", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/GulabJamun.jpeg", ytLink: "" },
-    { name: "Rasmalai", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Rasmalai.jpeg", ytLink: "" },
-    { name: "Kulfi", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Kulfi.jpeg", ytLink: "" }
+    { name: "Gulab Jamun", description: "A popular Indian dessert made from deep-fried dough balls soaked in a sugary syrup, often flavored with cardamom and rose water.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/GulabJamun.jpeg", ytLink: "" },
+    { name: "Rasmalai", description: "A creamy Indian dessert made from soft cheese dumplings soaked in sweetened, saffron-infused milk.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Rasmalai.jpeg", ytLink: "" },
+    { name: "Kulfi", description: "A traditional Indian frozen dessert similar to ice cream, made with thickened milk and flavored with cardamom, saffron, or pistachios.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Kulfi.jpeg", ytLink: "" }
 ];
 
 const japaneseMain = [
-    { name: "Ramen", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Ramen.jpeg", ytLink: "" },
-    { name: "Teriyaki Chicken Rice", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/TeriyakiChickenRice.webp", ytLink: "" },
-    { name: "Udon", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Udon.jpeg", ytLink: "" },
-    { name: "Katsu", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Katsu.jpeg", ytLink: "" }
+    { name: "Ramen", description: "A Japanese noodle soup dish featuring wheat noodles served in a flavorful broth, often topped with sliced pork, vegetables, and a boiled egg.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Ramen.jpeg", ytLink: "" },
+    { name: "Teriyaki Chicken Rice", description: "Grilled or pan-fried chicken marinated in a sweet and savory teriyaki sauce, served over steamed rice.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/TeriyakiChickenRice.webp", ytLink: "" },
+    { name: "Udon", description: "Thick wheat flour noodles commonly used in Japanese cuisine, served in various hot or cold dishes with broth or sauce.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Udon.jpeg", ytLink: "" },
+    { name: "Katsu", description: "A Japanese dish consisting of breaded and deep-fried cutlets of meat, typically pork or chicken.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Katsu.jpeg", ytLink: "" }
 ];
 const japaneseDrink = [
     { name: "Amazake", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Amazake.jpeg", ytLink: "" },
     { name: "Sake", description: "A Japanese rice wine with a diverse range of flavors and aromas, often served chilled or warmed.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Sake.webp", ytLink: "" },
-    { name: "Matcha Green Tea", description: "", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Matcha.jpeg", ytLink: "" },
+    { name: "Matcha Green Tea", description: "A traditional Japanese beverage crafted from shade-grown green tea leaves, meticulously ground into a vibrant green powder, celebrated for its rich umami flavor, potent antioxidants, and calming effects.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Matcha.jpeg", ytLink: "" },
     { name: "Royal Milk Tea", description: "A popular Japanese beverage made by blending black tea with milk and sweetening it with sugar or honey.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/RoyalMilkTea.webp", ytLink: "" }
 ];
 const japaneseSnack = [
-    { name: "Onigiri", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Onigiri.jpeg", ytLink: "" },
+    { name: "Onigiri", description: "Japanese rice balls often filled with savory ingredients such as pickled plum, salmon, or tuna, and wrapped in seaweed.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Onigiri.jpeg", ytLink: "" },
     { name: "Caramel Corn", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/CaramelCorn.png", ytLink: "" },
-    { name: "Pocky", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Pocky.jpeg", ytLink: "" },
+    { name: "Pocky", description: "A popular Japanese snack consisting of biscuit sticks dipped in chocolate or other flavored coatings.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Pocky.jpeg", ytLink: "" },
     { name: "Rice Crackers", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/RiceCrackers.jpeg", ytLink: "" }
 ];
 const japaneseDessert = [
@@ -69,8 +69,8 @@ const japaneseDessert = [
 ];
 
 const mexicanMain = [
-    { name: "Tamale", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Tamales.jpeg", ytLink: "" },
-    { name: "Burrito", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Burrito.webp", ytLink: "" },
+    { name: "Tamale", description: "A traditional Latin American dish made of masa dough filled with various ingredients such as meats, cheeses, or vegetables, wrapped in corn husks and steamed.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Tamales.jpeg", ytLink: "" },
+    { name: "Burrito", description: "A Mexican dish consisting of a flour tortilla wrapped around a filling of beans, meat, cheese, rice, and other ingredients.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Burrito.webp", ytLink: "" },
     { name: "Quesadilla", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Quesadilla.webp", ytLink: "" },
     { name: "Enchiladas", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Enchiladas.webp", ytLink: "" }
 ];
@@ -91,10 +91,10 @@ const mexicanDessert = [
 ];
 
 const westernMain = [
-    { name: "Meat Pie", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/MeatPie.jpeg", ytLink: "" },
-    { name: "Steak", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Steak.jpeg", ytLink: "" },
-    { name: "Turkey", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Turkey.jpeg", ytLink: "" },
-    { name: "Fish and Chips", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/FishAndChips.webp", ytLink: "" }
+    { name: "Meat Pie", description: "A savoury pie filled with a mixture of minced meat, gravy, and often vegetables, enclosed in pastry crust.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/MeatPie.jpeg", ytLink: "" },
+    { name: "Steak", description: "A tender and flavorful cut of beef, typically grilled or pan-seared to perfection.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Steak.jpeg", ytLink: "" },
+    { name: "Turkey", description: "A festive dish commonly served as the centerpiece of Thanksgiving or Christmas meals, prepared by roasting a whole turkey until golden brown and juicy.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Turkey.jpeg", ytLink: "" },
+    { name: "Fish and Chips", description: "A classic British dish featuring battered and deep-fried fish served with crispy fries, often accompanied by tartar sauce and malt vinegar.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/FishAndChips.webp", ytLink: "" }
 ];
 const westernDrink = [
     { name: "Hot Chocolate", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/HotChocolate.jpeg", ytLink: "" },
@@ -102,8 +102,8 @@ const westernDrink = [
     { name: "Root Beer", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/RootBeer.webp", ytLink: "" }
 ];
 const westernSnack = [
-    { name: "Salad", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Salad.jpeg", ytLink: "" },
-    { name: "Maple Pancakes", description: "test", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/MaplePancakes.jpeg", ytLink: "" },
+    { name: "Salad", description: "A dish typically consisting of mixed greens, vegetables, and often additional ingredients like nuts, cheese, or proteins, served with dressing.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/Salad.jpeg", ytLink: "" },
+    { name: "Maple Pancakes", description: "Fluffy pancakes topped with maple syrup, a classic breakfast dish popular in North America.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/MaplePancakes.jpeg", ytLink: "" },
 ];
 const westernDessert = [
     { name: "Chocolate Lava Cake", description: "A decadent dessert featuring a rich chocolate cake with a gooey, molten chocolate center.", imageSrc: "https://raw.githubusercontent.com/PravinRram/YDCP-WebDevProject/main/NomNomNavigator/images/ChocolateLavaCake.jpeg", ytLink: "" },
