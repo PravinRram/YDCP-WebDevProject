bb91a496cd753cda3fbc8dfa616a1e4c42468491
Pravin <144076247+PravinRram@users.noreply.github.com>
2024-03-14 15:46:57 +0800

    Added Menu feature to Header for smaller screens

--- NomNomNavigator/script.js
+++ NomNomNavigator/script.js
@@ -186,3 +186,15 @@ function getFoodOptions(main, drink, snack, dessert, foodType) {
             return [];
     }
 }
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+
+    sidebar.style.display = 'none';
+}
